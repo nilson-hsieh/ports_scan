d65d3ac98aeb89df7a53b0313937598d3134a551
nilson-hsieh <52262774+nilson-hsieh@users.noreply.github.com>
2020-04-09 00:53:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
                     }
                   }
                   $port = array(21,23,25,79,80,110,135,137,138,139,143,443,445,1433,3306);
-                  $msg = array('Ftp','Telnet','Smtp','Finger','Http','Pop3','Location Service','Netbios-NS','Netbios-DGM','Netbios-SSN','IMAP','Https','Microsoft-DS','MSSQL','MYSQL','Terminal Services');
+                  $msg = array('Ftp','Telnet','Smtp','Finger','Http','Pop3','Location Service','Netbios-NS','Netbios-DGM','Netbios-SSN','IMAP','Https','Microsoft-DS','SQL','SQL');
                   foreach ($port as $key => $value) {
                     echo '<tr>';
                     echo '<td>' . $key . '</td>';
